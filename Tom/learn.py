905bd66be4275f11f795a583d223975c7269d941
Grunzehund <jannik@cssbook.de>
2020-03-08 02:36:35 +0100

    WIP: some little modifications to make it learn
    Signed-off-by: Grunzehund <jannik@cssbook.de>

--- Tom/learn.py
+++ Tom/learn.py
@@ -10,8 +10,8 @@ import tensorflow as tf
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
 from tensorflow.keras.models import Sequential
 
-SHUFFLE_BUFFER_SIZE = 10
-BATCH_BUFFER_SIZE = 100
+SHUFFLE_BUFFER_SIZE = 1
+BATCH_BUFFER_SIZE = 1
 
 print("TensorFlow version: {}".format(tf.__version__))
 print("Eager execution: {}".format(tf.executing_eagerly()))
@@ -28,10 +28,12 @@ def LoadModels(_p, _i):
     print("Loaded " + str(len(train)) + " images")
     labels = [_i] * len(train)
 
-    train_test =   np.expand_dims(np.divide(np.asarray(train[:50]), 255.0), axis=3)
-    labels_test =  np.asarray(labels[:50])
-    train_train =  np.expand_dims(np.divide(np.asarray(train[50:]), 255.0), axis=3)
-    labels_train = np.asarray(labels[50:])
+    numtrain = int(len(labels) * 0.1)
+
+    train_test =   np.expand_dims(np.divide(np.asarray(train[:numtrain]), 255.0), axis=3)
+    labels_test =  np.asarray(labels[:numtrain])
+    train_train =  np.expand_dims(np.divide(np.asarray(train[numtrain:]), 255.0), axis=3)
+    labels_train = np.asarray(labels[numtrain:])
 
     return labels_test, labels_train, train_test, train_train
 
@@ -44,8 +46,6 @@ print(str(len(np.append(good_train_in, bad_train_in, axis=0))) + " / " + str(len
 train = tf.data.Dataset.from_tensor_slices((np.append(good_train_in, bad_train_in, axis=0), np.append(good_train_out, bad_train_out, axis=0))).shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_BUFFER_SIZE)
 test = tf.data.Dataset.from_tensor_slices((np.append(good_test_in, bad_test_in, axis=0), np.append(good_test_out, bad_test_out, axis=0))).shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_BUFFER_SIZE)
 
-# print(test_in)
-# sys.exit(0)
 
 model = Sequential([
     Conv2D(16, 3, data_format='channels_last', padding='same', activation='relu', input_shape=(128, 128, 1)),
@@ -61,18 +61,17 @@ model = Sequential([
 
 model.summary()
 
-
-model.compile(optimizer=tf.keras.optimizers.RMSprop(),
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        metrics=['sparse_categorical_accuracy'])
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
 
 save_callback = tf.keras.callbacks.ModelCheckpoint(filepath="model/",
                                                    save_weights_only=True,
                                                    verbose=1)
 
 
-for i in range(1):
-    model.fit(train, epochs=2, callbacks=[save_callback])
+for i in range(10):
+    model.fit(train, epochs=1000, callbacks=[save_callback])
 
 test_loss, test_acc = model.evaluate(test, verbose=0)
 
